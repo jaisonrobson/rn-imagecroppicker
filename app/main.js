ba265e5967b8a15d15a2c8c476f29c92c59aedfa
jaisonrobson <xd_jrg@hotmail.com>
2022-02-25 17:34:49 -0300

    feat: feito funcionalidade com a camera & alterado permissoes do projeto

--- app/main.js
+++ app/main.js
@@ -1,19 +1,102 @@
-import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native'
 import ImagePicker from 'react-native-image-crop-picker';
 
 const Main = () => {
+    const [image, setImage] = useState({})
+    const [errorText, setErrorText] = useState("")
+
+    const onSucessfulCropImage = (resultImage) => {
+        console.log('resultImage', resultImage)
+        setImage(resultImage)
+        setErrorText("")
+    }
+
+    const onFailCropImage = () => {
+        setImage({})
+        setErrorText("Falha ao carregar imagem.")
+    }
+
+    const getImageFromPath = () => {
+        if (image !== {} && image.path !== null) {
+            return { uri: image.path }
+        }
+
+        return null
+    }
+
+    const getImageStyle = () => {
+        if (getImageFromPath() !== null) {
+            return {
+                width: image.width,
+                height: image.height,
+                resizeMode: 'stretch',
+            }
+        }
+
+        return {}
+    }
+
+
+
     return (
-        <View>
-            <TouchableOpacity onPress={() => ImagePicker.openPicker({
-                width: 300,
-                height: 400,
-                cropping: true,
-            }).then(image => { console.log('imagem', image) })}>
-                <Text>Imagem</Text>
+        <View
+            style={{
+                flex: 1,
+            }}
+        >
+            <Image
+                style={{ ...styles.image, ...getImageStyle() }}
+                source={getImageFromPath()}
+            />
+
+            <TouchableOpacity
+                style={styles.cropImageButton}
+                onPress={() => ImagePicker.openPicker({
+                    width: 600,
+                    height: 600,
+                    cropping: true,
+                }).then(onSucessfulCropImage, onFailCropImage)}
+            >
+                <Text>Escolher imagem</Text>
             </TouchableOpacity>
+
+            <TouchableOpacity
+                style={styles.cropImageButton}
+                onPress={() => ImagePicker.openCamera({
+                    width: 600,
+                    height: 600,
+                    cropping: true,
+                }).then(onSucessfulCropImage, onFailCropImage)}
+            >
+                <Text>Tirar foto</Text>
+            </TouchableOpacity>
+
+            {
+                errorText !== ""
+                    ? (
+                        <Text>{errorText}</Text>
+                    )
+                    : undefined
+            }
+
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    image: {
+        flex: 1,
+        height: 200,
+        width: 200,
+        alignSelf: 'center',
+        backgroundColor: 'pink',
+    },
+
+    cropImageButton: {
+        height: 75,
+        backgroundColor: 'lightblue',
+    },
+})
 
 export default Main
